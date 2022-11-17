1e5c56a1a5b39f9f0ce1732b0e12df4f06022f75
randy <tmps880305@gmail.com>
2022-11-17 00:55:09 -0500

    Section 6-78: Media Query using Styled-Components
    responsive with screen size

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,7 @@
-// import React from 'react';
 import styled from 'styled-components'
 
-// import './Button.css';
-
 const Button = styled.button`
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -12,6 +10,9 @@ const Button = styled.button`
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
 
+    @media (min-width: 768px){
+      width: auto;
+    }
 
     &:focus {
       outline: none;
@@ -26,12 +27,5 @@ const Button = styled.button`
 
 `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
 
 export default Button;
